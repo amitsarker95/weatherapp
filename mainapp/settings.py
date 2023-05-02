a30fecfdf0ea84c21c141d2df9d953302558364a
amit <amitsarker95@gmail.com>
2023-05-02 18:46:00 +0600

    replaced

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -122,9 +122,7 @@ MEDIA_URL = 'images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    ]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
--- weatherapi/urls.py
+++ weatherapi/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
    
 urlpatterns = [
     path('', home, name='home'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
